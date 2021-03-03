fa943dd05aab1a54efa38d5e9c1e445291eb4f30
ly_yong <ly_yong@qq.com>
2021-03-03 20:54:28 +0800

    将order的RPC服务端口提出到配置文件

--- rpc/ticketPool/Client/tp_test.go
+++ rpc/ticketPool/Client/tp_test.go
@@ -14,13 +14,42 @@ func Test_GetTicketNumber(t *testing.T) {
 		t.Error(err)
 	}
 	res, err := cli.GetTicketNumber(&ticketPoolRPC.GetTicketNumberRequest{
-		TrainId:        []uint32{2},
-		StartStationId: 1,
-		DestStationId:  4,
-		Date:           "2021-02-28",
+		Date: "2021-02-28",
+		Condition: []*ticketPoolRPC.GetTicketNumberRequest_Condition{
+			&ticketPoolRPC.GetTicketNumberRequest_Condition{
+				TrainId:        17416,
+				StartStationId: 3337,
+				DestStationId:  3326,
+			},
+		},
 	})
 	if err != nil {
 		t.Error(err)
 	}
 	t.Log(res.TrainsTicketInfo[0])
+}
+
+func Test_BuyTicket(t *testing.T) {
+	cli, err := NewClient()
+	if err != nil {
+		t.Error(err)
+	}
+	res, err := cli.GetTicket(&ticketPoolRPC.GetTicketRequest{
+		TrainId:        17416,
+		StartStationId: 3337,
+		DestStationId:  3326,
+		Date:           "2021-02-28",
+		Passengers: []*ticketPoolRPC.PassengerInfo{
+			&ticketPoolRPC.PassengerInfo{
+				PassengerId:   1,
+				PassengerName: "123",
+				SeatTypeId:    0,
+				ChooseSeat:    "A",
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(res.Tickets[0])
 }
